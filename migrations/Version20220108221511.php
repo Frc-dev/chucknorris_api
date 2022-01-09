a4190af8cb0f03e3a4e3ed10cde2e0cb9277a327
Francis <frodriguezelche@gmail.com>
2022-01-09 20:39:59 +0100

    Fixed migrations to avoid creating deprecated async messages table

--- migrations/Version20220108221511.php
+++ migrations/Version20220108221511.php
@@ -16,13 +16,11 @@ final class Version20220108221511 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE search_results (result_id VARCHAR(255) NOT NULL, search_id VARCHAR(255) NOT NULL, categories LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', created_at DATETIME NOT NULL, value VARCHAR(255) NOT NULL, INDEX search_idx (search_id), PRIMARY KEY(result_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE search_results');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
